9fb3ed0e6e4f50388230dc67f7b6c4c3cdcf3d6a
andreasbalzer <github@andreas-balzer.de>
2014-11-29 15:35:55 +0000

    added new speed buttons but did not wire them yet, renamed viewmodel items

---
+++ Micropolis.Shared/Converters/SpeedButtonToBackgroundConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Micropolis.Converters
+{
+    public class SpeedButtonToBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value != null && value is bool && (bool)value == true)
+            {
+                return new SolidColorBrush(Colors.LightGray);
+            }
+            return new SolidColorBrush(Colors.Transparent);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Micropolis.Shared/ViewModels/MainGamePageViewModel.cs
+++ Micropolis.Shared/ViewModels/MainGamePageViewModel.cs
@@ -9,6 +9,9 @@
 // See the README file, included in this distribution, for details.
 // Project website: http://code.google.com/p/micropolis/
 
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
 namespace Micropolis.ViewModels
 {
     using System;
@@ -19,54 +22,159 @@ namespace Micropolis.ViewModels
 
     public class MainGamePageViewModel : BindableBase
     {
-        private bool messagesVisible;
+        private bool _isMessagesVisible;
 
-        private bool miniMapVisible;
+        private bool _isMiniMapVisible;
+
+        private bool _isSpeedPause;
+        private bool _isSpeedSlow;
+        private bool _isSpeedNormal;
+        private bool _isSpeedFast;
+        private bool _isSpeedSuperFast;
 
         public MainGamePageViewModel()
         {
             ToggleMiniMapCommand = new DelegateCommand(ToggleMiniMap);
             ToggleMessagesCommand = new DelegateCommand(ToggleMessages);
+            SpeedPauseCommand = new DelegateCommand(SpeedPause);
+            SpeedSlowCommand = new DelegateCommand(SpeedSlow);
+            SpeedNormalCommand = new DelegateCommand(SpeedNormal);
+            SpeedFastCommand = new DelegateCommand(SpeedFast);
+            SpeedSuperFastCommand = new DelegateCommand(SpeedSuperFast);
         }
 
-        public bool MessagesVisible
+        public bool IsMessagesVisible
         {
             get
             {
-                return this.messagesVisible;
+                return this._isMessagesVisible;
             }
-
             set
             {
-                this.SetProperty(ref this.messagesVisible, value);
+                this.SetProperty(ref this._isMessagesVisible, value);
                 
             }
         }
 
-        public bool MiniMapVisible
+        public bool IsMiniMapVisible
         {
             get
             {
-                return this.miniMapVisible;
+                return this._isMiniMapVisible;
             }
 
             set
             {
-                this.SetProperty(ref this.miniMapVisible, value);
+                this.SetProperty(ref this._isMiniMapVisible, value);
+            }
+        }
+
+        public bool IsSpeedPause
+        {
+            get { return _isSpeedPause; }
+            set
+            {
+                this.SetProperty(ref this._isSpeedPause, value);
+            }
+        }
+
+        public bool IsSpeedSlow
+        {
+            get { return _isSpeedSlow; }
+            set
+            {
+                this.SetProperty(ref this._isSpeedSlow, value);
+            }
+        }
+
+        public bool IsSpeedNormal
+        {
+            get { return _isSpeedNormal; }
+            set
+            {
+                this.SetProperty(ref this._isSpeedNormal, value);
+            }
+        }
+
+        public bool IsSpeedFast
+        {
+            get { return _isSpeedFast; }
+            set
+            {
+                this.SetProperty(ref this._isSpeedFast, value);
+            }
+        }
+
+        public bool IsSpeedSuperFast
+        {
+            get { return _isSpeedSuperFast; }
+            set
+            {
+                this.SetProperty(ref this._isSpeedSuperFast, value);
             }
         }
 
         public DelegateCommand ToggleMiniMapCommand { get; private set; }
         public DelegateCommand ToggleMessagesCommand { get; private set; }
-
+        public DelegateCommand SpeedPauseCommand { get; private set; }
+        public DelegateCommand SpeedSlowCommand { get; private set; }
+        public DelegateCommand SpeedNormalCommand { get; private set; }
+        public DelegateCommand SpeedFastCommand { get; private set; }
+        public DelegateCommand SpeedSuperFastCommand { get; private set; }
         private void ToggleMiniMap()
         {
-            MiniMapVisible = !MiniMapVisible;
+            IsMiniMapVisible = !IsMiniMapVisible;
         }
 
         private void ToggleMessages()
         {
-            MessagesVisible = !MessagesVisible;
+            IsMessagesVisible = !IsMessagesVisible;
+        }
+
+
+        private void SpeedPause()
+        {
+            IsSpeedSlow = false;
+            IsSpeedNormal = false;
+            IsSpeedFast = false;
+            IsSpeedSuperFast = false;
+            IsSpeedPause = true;
+        }
+
+        private void SpeedSlow()
+        {
+            IsSpeedPause = false;
+            IsSpeedNormal = false;
+            IsSpeedFast = false;
+            IsSpeedSuperFast = false;
+            IsSpeedSlow = true;
+        }
+
+        private void SpeedNormal()
+        {
+            IsSpeedPause = false;
+            IsSpeedSlow = false;
+            IsSpeedFast = false;
+            IsSpeedSuperFast = false;
+            IsSpeedNormal = true;
+        }
+
+        private void SpeedFast()
+        {
+            IsSpeedPause = false;
+            IsSpeedSlow = false;
+            IsSpeedNormal = false;
+            IsSpeedSuperFast = false;
+            IsSpeedFast = true;
+        }
+
+        private void SpeedSuperFast()
+        {
+            IsSpeedPause = false;
+            IsSpeedSlow = false;
+            IsSpeedNormal = false;
+            IsSpeedFast = false;
+            IsSpeedSuperFast = true;
         }
     }
 }
