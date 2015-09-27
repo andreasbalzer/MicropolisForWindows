fb9572613ef1919b26984b5c3daeac2ed89a39de
andreasbalzer <git@andreas-balzer.de>
2015-09-27 21:37:51 +0100

    adding missing files to depot

---
+++ Micropolis.W10/Utils/BackButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace Micropolis.Utils
+{
+    public static class BackButton
+    {
+        public static void RegisterBackButton(Frame frame)
+        {
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            Windows.UI.Core.SystemNavigationManager.GetForCurrentView().BackRequested += (s, a) =>
+            {
+                if (frame.CanGoBack)
+                {
+                    frame.GoBack();
+                    a.Handled = true;
+                }
+            };
+        }
+    }
+}
